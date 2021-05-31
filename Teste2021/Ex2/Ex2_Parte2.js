d4d6bc5d6f6c38f3d62e31a2446e1b3d0ef64182
JoaoGome <jprodriguesgomespoe@gmail.com>
2021-05-31 13:30:36 +0100

    Teste

--- Teste2021/Ex2/Ex2_Parte2.js
+++ Teste2021/Ex2/Ex2_Parte2.js
@@ -1,16 +1,31 @@
-axios = require('axios');
-
-repoID = "Ex2";
-
-var getLink = "http://localhost:7200/repositories/" + repoID + "?query=" ;
+var axios = require('axios')
 var prefixes = `
-  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-  PREFIX owl: <http://www.w3.org/2002/07/owl#>
-  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-  PREFIX noInferences: <http://www.ontotext.com/explicit>
-  PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-  PREFIX : <http://prc.di.uminho.pt/2021/myfamily#>
-`
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX noInferences: <http://www.ontotext.com/explicit>
+        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+        PREFIX : <http://prc.di.uminho.pt/2021/myfamily#>
+        `
+
+execQuery = async function (query){
+    var getLink = "http://localhost:7200/repositories/Ex2?query="
+    var encoded = encodeURIComponent(prefixes + query)
+    var result = await axios.get(getLink + encoded)
+    return result.data
+}
+
+execTransaction = async function(query){
+    var postLink = "http://localhost:7200/repositories/Ex2/statements"
+    var encoded = encodeURIComponent(prefixes + query)
+    var response
+    try{
+        response = await axios.post(postLink, `update=${encoded}`)
+        return response.data
+    }catch(error){
+        throw(error)
+    }
+}
 
 // usando a query construct da segunda alinea -- Constrói os triplos da relação Bisavô
 
@@ -26,24 +41,12 @@ var query = `
             }
               `;
 
-var encoded = encodeURIComponent(prefixes + query);
 
-axios.get(getLink + encoded)
-.then(dados => {
 
-    var query2 = `
-                INSERT DATA {
-                    ${dados}
-                }
-    `
-    var postLink = "http://localhost:7200/repositories/Ex2/statements"
-    var encoded2 = encodeURIComponent(prefixes + query2);
-    axios.post(postLink, `update=${encoded2}`)
-        .then(dados2 => {
-            console.log("Good"); 
-    })
-    .catch(erro => console.log(erro))
-
-})
-.catch(erro => console.log(erro));
-              
+execQuery(query).then( dados => {
+     query2 = `INSERT DATA {
+        ${dados}
+    }`
+    execTransaction(query2)
+}
+)
